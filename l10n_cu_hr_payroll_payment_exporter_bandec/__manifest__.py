021c5be295783aadae0cba5b8cd241d01a600095
Adrian Segura <asegura@lt.desoft.cu>
2021-12-27 14:56:14 -0500

    definiendo modulo base para exportar nominas al banco bandec.

---
+++ l10n_cu_hr_payroll_payment_exporter_bandec/__manifest__.py
@@ -0,0 +1,13 @@
+# List of contributors:
+# Segu S.U.R.L
+
+{
+     'name': 'Cuba - Módulo base para exportar nóminas a BANDEC',
+     'version': '0.1',
+     'category': 'Uncategorized',
+     'author': 'Comunidad Cubana de Odoo',
+     'depends': [],
+     'data': [
+
+     ],
+}
---
+++ l10n_cu_hr_payroll_payment_exporter_bandec/models/__init__.py
@@ -0,0 +1,3 @@
+from . import hr_payslip
+from . import hr_employee
+from . import hr_projection
