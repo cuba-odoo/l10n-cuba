ef2d09e45d5fc2b7c20f6b7d6a861460dedadb86
Adrian Segura <asegura@lt.desoft.cu>
2022-01-23 13:54:23 -0500

    [IMP] Mejorando funcionalidades de los reportes financieros (type account_reports. Para obtener el balance de varios sub reportes).

--- l10n_cu_reports/models/account_financial_report.py
+++ l10n_cu_reports/models/account_financial_report.py
@@ -4,14 +4,17 @@ from odoo import fields, models, api, _
 class AccountFinancialReport(models.Model):
     _inherit = 'account.financial.report'
 
-    plan_anual = fields.Float("Plan Anual", help="Lo que se definión en el presupuesto.")
+    plan_anual = fields.Float("Plan Anual", help="Lo que se definió en el presupuesto.")
     apertura = fields.Float("Apertura")
     visible = fields.Boolean('Visible', default=True)
     type = fields.Selection(selection=[
         ('sum', 'View'),
         ('accounts', 'Accounts'),
-        ('account_group', 'Account Group'),
+        # ('account_group', 'Account Group'),
         ('account_type', 'Account Type'),
         ('account_report', 'Report Value'),
+        ('account_reports', 'Reports Value'),
     ])
+    account_report_ids = fields.Many2many('account.financial.report', 'rel_account_report', 'account_report_id',
+                                          'account_report_rel_id', string='Report Value', help="Hola")
 
--- l10n_cu_reports/reports/report_financial.py
+++ l10n_cu_reports/reports/report_financial.py
@@ -5,6 +5,52 @@ from odoo import api, models, _
 class ReportFinancial(models.AbstractModel):
     _inherit = 'report.accounting_pdf_reports.report_financial'
 
+    def _compute_report_balance(self, reports):
+        '''returns a dictionary with key=the ID of a record and value=the credit, debit and balance amount
+           computed for this record. If the record is of type :
+               'accounts' : it's the sum of the linked accounts
+               'account_type' : it's the sum of leaf accoutns with such an account_type
+               'account_report' : it's the amount of the related report
+               'sum' : it's the sum of the children of this record (aka a 'view' record)'''
+        res = {}
+        fields = ['credit', 'debit', 'balance']
+        for report in reports:
+            if report.id in res:
+                continue
+            res[report.id] = dict((fn, 0.0) for fn in fields)
+            if report.type == 'accounts':
+                # it's the sum of the linked accounts
+                res[report.id]['account'] = self._compute_account_balance(report.account_ids)
+                for value in res[report.id]['account'].values():
+                    for field in fields:
+                        res[report.id][field] += value.get(field)
+            elif report.type == 'account_type':
+                # it's the sum the leaf accounts with such an account type
+                accounts = self.env['account.account'].search([('user_type_id', 'in', report.account_type_ids.ids)])
+                res[report.id]['account'] = self._compute_account_balance(accounts)
+                for value in res[report.id]['account'].values():
+                    for field in fields:
+                        res[report.id][field] += value.get(field)
+            elif report.type == 'account_report' and report.account_report_id:
+                # it's the amount of the linked report
+                res2 = self._compute_report_balance(report.account_report_id)
+                for key, value in res2.items():
+                    for field in fields:
+                        res[report.id][field] += value[field]
+            elif report.type == 'account_reports' and report.account_report_ids:
+                # it's the amount of the linked reports
+                res2 = self._compute_report_balance(report.account_report_ids)
+                for key, value in res2.items():
+                    for field in fields:
+                        res[report.id][field] += value[field]
+            elif report.type == 'sum':
+                # it's the sum of the children of this account.report
+                res2 = self._compute_report_balance(report.children_ids)
+                for key, value in res2.items():
+                    for field in fields:
+                        res[report.id][field] += value[field]
+        return res
+
     def get_account_lines(self, data):
         lines = []
         account_report = self.env['account.financial.report'].search([('id', '=', data['account_report_id'][0])])
