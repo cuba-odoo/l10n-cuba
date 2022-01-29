84fc8202cd50bba519237bdac0cbbf980a789982
Adrian Segura Trista <asegurat15@gmail.com>
2022-01-29 13:36:55 -0500

    [IMP] Mejorando validaciones para las actividades econominas (CNAE).

--- l10n_cu/models/res_company.py
+++ l10n_cu/models/res_company.py
@@ -3,18 +3,29 @@
 
 from odoo import models, fields
 
+
 class ResCnae(models.Model):
     _name = 'res.cnae'
+
+    def name_get(self):
+        return [(record.id, "%s - %s" % (record.code, record.name)) for record in self]
 
     code = fields.Char()
     name = fields.Char()
 
+
 class ResCnaeLine(models.Model):
     _name = 'res.cnae.line'
+    _sql_constraints = [
+        ('check_cnae_id', 'unique(cnae_id)',
+         'Las actividades economicas deben ser unicas.!')
+    ]
 
-    sequence = fields.Integer('Sequence', default=1, help="Usado para organizar las actividades. La Actividad Principal sera la de primer orden.")
+    sequence = fields.Integer('Sequence', default=1,
+                              help="Usado para organizar las actividades. La Actividad Principal sera la de primer orden.")
     cnae_id = fields.Many2one("res.cnae", "Clasificador Nacional de Activdades Economicas", required=True)
     company_id = fields.Many2one("res.company", "Company", required=True)
+
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
