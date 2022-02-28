d2eed06e5238a7985eb188252cf55210dcaa7dc4
Adrian Segura Trista <asegurat15@gmail.com>
2022-02-28 17:10:20 -0500

    [15.0][IMP] Asignando 2100 como valor por defecto del salario minimo.

--- l10n_cu_hr_payroll/models/res_config_settings.py
+++ l10n_cu_hr_payroll/models/res_config_settings.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    min_salary = fields.Char(string='Salario mínimo', config_parameter='l10n_cu_hr_payroll.default_min_salary')
-
+    min_salary = fields.Char(string='Salario mínimo', config_parameter='l10n_cu_hr_payroll.default_min_salary',
+                             default=2100, required=True)
