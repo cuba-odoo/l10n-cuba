494d0a8e70da786a10e541b4b72c02d230049192
Adrian Segura Trista <asegurat15@gmail.com>
2022-04-25 10:08:53 -0400

    [15.0][IMP] Homologando prefijos plan contable del sector estatal.

--- l10n_cu_reports/reports/report_financial.py
+++ l10n_cu_reports/reports/report_financial.py
@@ -7,12 +7,7 @@ class ReportFinancial(models.AbstractModel):
     _inherit = 'report.accounting_pdf_reports.report_financial'
 
     def _compute_report_balance(self, reports):
-        '''returns a dictionary with key=the ID of a record and value=the credit, debit and balance amount
-           computed for this record. If the record is of type :
-               'accounts' : it's the sum of the linked accounts
-               'account_type' : it's the sum of leaf accoutns with such an account_type
-               'account_report' : it's the amount of the related report
-               'sum' : it's the sum of the children of this record (aka a 'view' record)'''
+        # Overwrite
         res = {}
         fields = ['credit', 'debit', 'balance']
         for report in reports:
@@ -53,6 +48,7 @@ class ReportFinancial(models.AbstractModel):
         return res
 
     def get_account_lines(self, data):
+        # Overwrite
         lines = []
         account_report = self.env['account.financial.report'].search([('id', '=', data['account_report_id'][0])])
         child_reports = account_report._get_children_by_order()
