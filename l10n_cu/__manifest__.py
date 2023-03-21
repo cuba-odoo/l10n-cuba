2e5e97fb3e240a4bd933e7645de4e96c5e5cc7b2
Hanoi Calvo Fernandez <scnetisla@gmail.com>
2023-03-21 00:46:00 -0400

    Revert "[ADD] Migrando modulo l10n_cu de la V15.0 a la V16.0"
    This reverts commit 02a5b6b1a0831afc43593f2557fbd0a5ce8f86f4.

--- l10n_cu/__manifest__.py
+++ l10n_cu/__manifest__.py
@@ -4,14 +4,13 @@
 # List of contributors:
 # Bernardo Yaser León Ávila <bernardo@idola.it>
 # Yunior Rafael Hernández Cabrera <yunior@idola.it>
-# Fidel Jimenez Sanzano <fjsanzano85@gmail.com>
 # Yusnel Rojas Garcia
 # Julio Smith
 # Segu
 
 {
     'name': 'Cuba - Contabilidad',
-    'version': '16.0',
+    'version': '15.0',
     'author': 'Idola Odoo Team, Comunidad cubana de Odoo ',
     'category': 'Accounting/Localizations/Account Charts',
     'description': """
