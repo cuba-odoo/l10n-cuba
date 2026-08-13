c361797e1c3ee877a5dee5bf94583e6a1671c7d1
Rafael Rubén Jorge González <rrjorgegz@gmail.com>
2026-08-13 12:23:54 -0400

    [ADD] l10n_cu_hr_payroll_account: add module from branch 17.0

---
+++ l10n_cu_hr_payroll_account/__manifest__.py
@@ -0,0 +1,20 @@
+# List of contributors:
+# Segu
+
+{
+    'name': 'Cuba - Nóminas con contabilidad',
+    'category': 'Accounting/Accounting',
+    'version': '18.0',
+    'description': """
+Accounting Data for Cuba Payroll Rules.
+==========================================
+    """,
+    'depends': ['om_hr_payroll_account', 'l10n_cu', 'l10n_cu_hr_payroll'],
+    'author': 'Comunidad Cubana de Odoo',
+    'data': [
+        'data/hr_payroll_account_data.xml',
+        'views/hr_payroll_account_views.xml',
+        "wizard/hr_payroll_payslips_by_employees_wizard.xml",
+    ],
+    'license': 'LGPL-3',
+}
