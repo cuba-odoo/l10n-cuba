c75cd5539a0b8030b154b05c8e91d2c64b52d5eb
Rafael Rubén Jorge González <rrjorgegz@gmail.com>
2026-08-13 12:23:02 -0400

    [ADD] l10n_cu_hr_payroll: add module from branch 17.0

---
+++ l10n_cu_hr_payroll/__manifest__.py
@@ -0,0 +1,31 @@
+# List of contributors:
+# Segu
+
+{
+     'name': 'Cuba - Nóminas',
+     'version': '18.0',
+     'category': 'Human Resources',
+     'summary': """
+        Estructuras y reglas salariales, proyecciones de salarios.
+     """,
+     'author': 'Comunidad Cubana de Odoo',
+     'depends': [
+          "l10n_cu_hr",
+          "l10n_cu_hr_contract",
+          "om_hr_payroll",
+     ],
+     'data': [
+          "data/hr_payroll_data.xml",
+          "data/hr.salary.rule.csv",
+          "security/ir.model.access.csv",
+          "views/hr_payslip_views.xml",
+          "views/hr_employee_views.xml",
+          "views/hr_projection_views.xml",
+          "views/report_projection_template.xml",
+          "views/report_payslip_run_template.xml",
+          "views/res_config_settings_views.xml",
+          "reports/hr_payroll_report.xml",
+          "wizard/hr_payroll_projection_wizard.xml",
+     ],
+     'license': 'LGPL-3',
+}
